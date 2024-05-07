910949c36fa54f1784569a349b213a75f3b0cfeb
Iryna Kakalets <irinakakalec@gmail.com>
2024-05-07 18:57:03 +0200

    Uloha1 done

--- src/components/Uloha1/index.jsx
+++ src/components/Uloha1/index.jsx
@@ -1,7 +1,9 @@
 /*
 Zadání: Po kliku na tlačítko zobrazte alert s textem „Ahoj“.
 */
+import React from 'react';
 
 export const Uloha1 = () => {
-  return <button>Ukaž bublinu</button>;
+  const showBubble = () => alert('Ahoj');
+  return <button onClick={showBubble}>Ukaž bublinu</button>;
 };
